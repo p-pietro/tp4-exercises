041700ef6a32f8d09f2a494413889318ff466710
Pietro <90720953+p-pietro@users.noreply.github.com>
2024-11-02 16:59:40 +0100

    Speed up through ode solver

--- python/functions.py
+++ python/functions.py
@@ -1,11 +1,32 @@
 import scipy
 import numpy as np
+from scipy.integrate import solve_ivp, ode
 
 
-def integrate(H, dt, psi0):
-    psi = psi0
-    psi = scipy.linalg.expm(-1j * H * dt) @ psi
-    return psi
+def schrodinger_eq(t, psi, H):
+    dpsi_dt = -1j * H @ psi
+    return dpsi_dt
+
+
+class Integrator:
+    def __init__(self):
+        self.solver = ode(schrodinger_eq).set_integrator("zvode", method="adams")
+
+    def integrate(self, H, psi0, t_span, t_eval):
+        # Solve the Schrödinger equation using the ode function with the zvode solver
+        if (t_eval < t_span[0]) or (t_eval > t_span[1]):
+            raise ValueError("t_eval must be within t_span")
+
+        self.solver.set_initial_value(psi0, t_span[0]).set_f_params(H)
+
+        psi_t = psi0
+        if self.solver.successful():
+            self.solver.integrate(t_eval)
+            psi_t = self.solver.y
+        else:
+            raise RuntimeError("ODE solver failed to integrate")
+
+        return psi_t
 
 
 def determine_jump(c_ops, psi, r2):
@@ -21,20 +42,34 @@ def determine_jump(c_ops, psi, r2):
     return jump
 
 
-def montecarlo(H, c_ops, psi0, tlist):
+def montecarlo(H, c_ops, psi0, tlist, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+
+    integrator = Integrator()
+
     H_eff = H - 1j * sum([c.conj().T @ c for c in c_ops]) / 2
 
     dt = (max(tlist) - min(tlist)) / len(tlist)
     psi = psi0 / np.linalg.norm(psi0)
     psi_j = []
     psi_j.append(psi)
-
+    t_prev = tlist[0]
     r1 = np.random.rand()
     r2 = np.random.rand()
-    for t_e in tlist:
-        psi = integrate(H_eff, dt, psi)
 
-        if np.linalg.norm(psi) ** 2 <= r1:
+    for t_idx in range(1, len(tlist)):
+        t_span = (t_prev, tlist[t_idx])
+        t_eval = tlist[t_idx]
+
+        # Integrate using the ODE solver
+        psi = integrator.integrate(H_eff, psi, t_span, t_eval)
+        # psi = psi_t.y[:, -1]
+
+        t_prev = tlist[t_idx]
+
+        norm_sq = np.linalg.norm(psi) ** 2
+        if norm_sq <= r1:
             jump = determine_jump(c_ops, psi, r2)
             psi = jump @ psi / np.linalg.norm(jump @ psi)
             r1 = np.random.rand()
@@ -42,7 +77,6 @@ def montecarlo(H, c_ops, psi0, tlist):
 
         psi_j.append(psi)
 
-    psi_j.pop()
     return psi_j
 
 
